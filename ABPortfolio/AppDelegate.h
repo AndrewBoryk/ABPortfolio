6a60c84ee2c809017620827a021a3a679675b2b3
Andrew Boryk <aboryk@mercymavericks.edu>
2017-03-05 13:53:11 -0500

    Initial Commit

---
+++ ABPortfolio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ABPortfolio
+//
+//  Created by Andrew Boryk on 3/5/17.
+//  Copyright © 2017 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
